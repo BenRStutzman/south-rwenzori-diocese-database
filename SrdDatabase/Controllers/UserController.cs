977defb86f7e98c7d0ed4ea74051a540cd69cedf
BenRStutzman <tysorx89@gmail.com>
2021-11-11 10:58:24 +0300

    Add user controller

--- SrdDatabase/Controllers/UserController.cs
+++ SrdDatabase/Controllers/UserController.cs
@@ -1,14 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SrdDatabase.Models.User;
+using SrdDatabase.Services;
 
 namespace SrdDatabase.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UserController
+    public class UserController : Controller
     {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest request)
+        {
+            var response = _userService.Authenticate(request);
+
+            if (response == null)
+            {
+                return BadRequest("Username or password is incorrect");
+            }
+
+            return Ok(response);
+        }
     }
 }
