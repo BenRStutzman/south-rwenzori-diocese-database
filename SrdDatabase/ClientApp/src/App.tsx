a240c30098890233257294726002bf50f515f6c3
BenRStutzman <tysorx89@gmail.com>
2021-11-15 10:18:58 +0300

    Set up private routes

--- SrdDatabase/ClientApp/src/App.tsx
+++ SrdDatabase/ClientApp/src/App.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import './site.css'
-import Home from './components/Home';
 import Login from './components/Login';
+import Home from './components/Home';
 import ArchdeaconryHome from './components/archdeaconry/Home';
 import ArchdeaconryAdd from './components/archdeaconry/Add';
 import ArchdeaconryEdit from './components/archdeaconry/Edit';
@@ -16,22 +16,23 @@ import CongregationEdit from './components/congregation/Edit';
 import EventSearch from './components/event/Home';
 import EventAdd from './components/event/Add';
 import EventEdit from './components/event/Edit';
+import PrivateRoute from './components/shared/PrivateRoute';
 
 export default () => (
     <Layout>
-        <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
-        <Route exact path='/archdeaconry' component={ArchdeaconryHome} />
-        <Route exact path='/archdeaconry/add' component={ArchdeaconryAdd} />
-        <Route exact path='/archdeaconry/edit/:archdeaconryId' component={ArchdeaconryEdit} />
-        <Route exact path='/parish' component={ParishSearch} />
-        <Route exact path='/parish/add' component={ParishAdd} />
-        <Route exact path='/parish/edit/:parishId' component={ParishEdit} />
-        <Route exact path='/congregation' component={CongregationSearch} />
-        <Route exact path='/congregation/add' component={CongregationAdd} />
-        <Route exact path='/congregation/edit/:congregationId' component={CongregationEdit} />
-        <Route exact path='/event' component={EventSearch} />
-        <Route exact path='/event/add' component={EventAdd} />
-        <Route exact path='/event/edit/:eventId' component={EventEdit} />
+        <PrivateRoute exact path='/' component={Home} />
+        <PrivateRoute exact path='/archdeaconry' component={ArchdeaconryHome} />
+        <PrivateRoute exact path='/archdeaconry/add' component={ArchdeaconryAdd} />
+        <PrivateRoute exact path='/archdeaconry/edit/:archdeaconryId' component={ArchdeaconryEdit} />
+        <PrivateRoute exact path='/parish' component={ParishSearch} />
+        <PrivateRoute exact path='/parish/add' component={ParishAdd} />
+        <PrivateRoute exact path='/parish/edit/:parishId' component={ParishEdit} />
+        <PrivateRoute exact path='/congregation' component={CongregationSearch} />
+        <PrivateRoute exact path='/congregation/add' component={CongregationAdd} />
+        <PrivateRoute exact path='/congregation/edit/:congregationId' component={CongregationEdit} />
+        <PrivateRoute exact path='/event' component={EventSearch} />
+        <PrivateRoute exact path='/event/add' component={EventAdd} />
+        <PrivateRoute exact path='/event/edit/:eventId' component={EventEdit} />
     </Layout>
 );
