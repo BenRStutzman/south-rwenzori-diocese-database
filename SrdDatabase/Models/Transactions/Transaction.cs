76e8836176ef714e9025e5c632c34fd622e653f3
BenRStutzman <tysorx89@gmail.com>
2021-12-10 16:14:08 +0300

    Add transaction models

---
+++ SrdDatabase/Models/Transactions/Transaction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SrdDatabase.Models.Trnasactions
+{
+    public class Transaction
+    {
+        public int Id { get; }
+
+        public sbyte TransactionTypeId { get; }
+
+        public string TransactionType { get; }
+
+        public int Amount { get; }
+
+        public int CongregationId { get; }
+
+        public string Congregation { get; }
+
+        public int ParishId { get; }
+
+        public string Parish { get; }
+
+        public int ArchdeaconryId { get; }
+
+        public string Archdeaconry { get; }
+
+        public DateTime Date { get; }
+
+        public Transaction(
+            int id,
+            int amount,
+            sbyte transactionTypeId,
+            string transationType,
+            int congregationId,
+            string congregation,
+            int parishId,
+            string parish,
+            int archdeaconryId,
+            string archdeaconry,
+            DateTime date)
+        {
+            Id = id;
+            Amount = amount;
+            TransactionTypeId = transactionTypeId;
+            TransactionType = transationType;
+            CongregationId = congregationId;
+            Congregation = congregation;
+            ParishId = parishId;
+            Parish = parish;
+            ArchdeaconryId = archdeaconryId;
+            Archdeaconry = archdeaconry;
+            Date = date;
+        }
+
+        // for Dapper
+        public Transaction()
+        {
+        }
+    }
+}
---
+++ SrdDatabase/Models/Transactions/TransactionType.cs
@@ -0,0 +1,20 @@
+﻿namespace SrdDatabase.Models.Transactions
+{
+    public class TransactionType
+    {
+        public sbyte Id { get; }
+
+        public string Name { get; }
+
+        public TransactionType(sbyte id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        // for Dapper
+        public TransactionType()
+        {
+        }
+    }
+}
