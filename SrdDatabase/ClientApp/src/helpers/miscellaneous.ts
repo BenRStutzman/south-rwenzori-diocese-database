541ced940977b90c80e795499e8001471fc5710a
BenRStutzman <tysorx89@gmail.com>
2022-01-03 12:14:42 +0300

    Remove unnecessary import

--- SrdDatabase/ClientApp/src/helpers/miscellaneous.ts
+++ SrdDatabase/ClientApp/src/helpers/miscellaneous.ts
@@ -1,6 +1,4 @@
-﻿import { Transaction } from "../models/transaction";
-
-export function randomString() {
+﻿export function randomString() {
     return Math.random().toString(36).substring(7);
 };
 
