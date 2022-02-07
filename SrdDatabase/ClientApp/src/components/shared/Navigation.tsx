64915b5f83ebefd5ba969aea8abf358c7197156e
BenRStutzman <tysorx89@gmail.com>
2022-02-07 13:24:29 +0300

    Show fuller nav bar for accountant

--- SrdDatabase/ClientApp/src/components/shared/Navigation.tsx
+++ SrdDatabase/ClientApp/src/components/shared/Navigation.tsx
@@ -5,7 +5,6 @@ import * as Store from '../../store/shared';
 import { State } from '../../store';
 import { connect } from 'react-redux';
 import { atLeast } from '../../helpers/userHelper';
-import { userRole } from '../../models/user';
 
 type Props = Store.State;
 
@@ -28,26 +27,20 @@ const Navigation = ({
                         currentUser &&
                         <Collapse className='d-sm-inline-flex flex-sm-row-reverse' isOpen={isOpen} navbar>
                             <ul className='navbar-nav flex-grow'>
-                                {
-                                    currentUser?.userType !== userRole.accountant &&
-                                    <>
-                                        <NavItem>
-                                            <NavLink tag={Link} className='text-dark' to='/archdeaconry'>Archdeaconries</NavLink>
-                                        </NavItem>
-                                        <NavItem>
-                                            <NavLink tag={Link} className='text-dark' to='/parish'>Parishes</NavLink>
-                                        </NavItem>
-                                    </>
-                                }
+                                <>
+                                    <NavItem>
+                                        <NavLink tag={Link} className='text-dark' to='/archdeaconry'>Archdeaconries</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink tag={Link} className='text-dark' to='/parish'>Parishes</NavLink>
+                                    </NavItem>
+                                </>
                                 <NavItem>
                                     <NavLink tag={Link} className='text-dark' to='/congregation'>Congregations</NavLink>
                                 </NavItem>
-                                {
-                                    currentUser?.userType !== userRole.accountant &&
-                                    <NavItem>
-                                        <NavLink tag={Link} className='text-dark' to='/event'>Events</NavLink>
-                                    </NavItem>
-                                }
+                                <NavItem>
+                                    <NavLink tag={Link} className='text-dark' to='/event'>Events</NavLink>
+                                </NavItem>
                                 {
                                     currentUser && atLeast.accountant.includes(currentUser.userType) &&
                                     <>
