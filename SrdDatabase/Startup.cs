111e4266c9924d3025038b21734d1d8992df50ea
BenRStutzman <tysorx89@gmail.com>
2022-03-31 10:23:21 +0300

    Remove error handling in production

--- SrdDatabase/Startup.cs
+++ SrdDatabase/Startup.cs
@@ -43,17 +43,6 @@ namespace SrdDatabase
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
