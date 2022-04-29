bf149619ccb28071e86dde296a0c4c85236c8730
BenRStutzman <tysorx89@gmail.com>
2022-04-29 13:18:45 +0300

    Add member js model

---
+++ SrdDatabase/ClientApp/src/models/sacco/member.ts
@@ -0,0 +1,18 @@
+﻿import { PagedParameters, PagedResults } from "../shared";
+
+export interface Member {
+    id?: number;
+    name?: string;
+}
+
+export interface MemberDetails {
+    member: Member;
+}
+
+export interface MemberParameters extends PagedParameters {
+    name?: string;
+}
+
+export interface MemberResults extends PagedResults {
+    archdeaconries: Member[];
+}
