aaae94e9d2d3c0d97cfeab7ca05287d2dd26f45a
BenRStutzman <tysorx89@gmail.com>
2022-05-02 15:59:03 +0300

    Add data commands and queries for transactions

---
+++ SrdDatabase/Data/Commands/Sacco/Transactions/SaveMember.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using SrdDatabase.Services;
+using SrdDatabase.Models.Sacco.Transactions;
+using SrdDatabase.Models.Shared;
+using System;
+
+namespace SrdDatabase.Data.Commands.Sacco.Transactions
+{
+    public class SaveTransaction
+    {
+        public class Command : TransactionFields, IRequest<SaveResponse>
+        {
+            public int? Id { get; set; }
+
+            public Command(
+                int? id,
+                int amount,
+                int memberId,
+                DateTime date,
+                bool isShares,
+                bool isContribution,
+                int receiptNumber,
+                int userId)
+                : base (amount, memberId, date, isShares, isContribution, receiptNumber, userId)
+            {
+                Id = id;
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, SaveResponse>
+        {
+            private readonly IDbService _dbService;
+
+            private readonly string _storedProcedure = "sto_save_sacco_transaction";
+
+            public Handler(IDbService dbService)
+            {
+                _dbService = dbService;
+            }
+
+            public async Task<SaveResponse> Handle(Command request, CancellationToken cancellationToken)
+            {
+                using var connection = _dbService.GetConnection();
+
+                var id = await connection.QuerySingleAsync<int>(
+                    _storedProcedure,
+                    request,
+                    commandType: CommandType.StoredProcedure);
+
+                return new SaveResponse(id);
+            }
+        }
+    }
+}
---
+++ SrdDatabase/Data/Queries/Sacco/Transactions/GetTransactions.cs
@@ -0,0 +1,73 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using SrdDatabase.Services;
+using SrdDatabase.Models.Sacco.Transactions;
+using System;
+
+namespace SrdDatabase.Data.Queries.Sacco.Transactions
+{
+    public class GetTransactions
+    {
+        public class Query : TransactionParameters, IRequest<TransactionResults>
+        {
+            public Query(
+                int? id = null,
+                int? congregationId = null,
+                DateTime? startDate = null,
+                DateTime? endDate = null,
+                int? receiptNumber = null,
+                bool? isShares = null,
+                bool? isContribution = null,
+                int pageNumber = 0,
+                string sortColumn = null,
+                bool sortDescending = false,
+                int? pageSize = null) :
+                base (congregationId,
+                    startDate,
+                    endDate,
+                    receiptNumber,
+                    isShares,
+                    isContribution,
+                    pageNumber,
+                    sortColumn,
+                    sortDescending,
+                    pageSize,
+                    id)
+            {
+            }
+        }
+
+        public class Handler : IRequestHandler<Query, TransactionResults>
+        {
+            private readonly IDbService _dbService;
+            private readonly string _storedProcedure = "sto_get_sacco_transactions";
+
+            public Handler(IDbService dbService)
+            {
+                _dbService = dbService;
+            }
+
+            public async Task<TransactionResults> Handle(Query request, CancellationToken cancellationToken)
+            {
+                using var connection = _dbService.GetConnection();
+
+                using var results = await connection.QueryMultipleAsync(
+                    _storedProcedure,
+                    request,
+                    commandType: CommandType.StoredProcedure);
+
+                var totalResults = results.ReadSingle<int>();
+                var transactions = results.Read<Transaction>();
+
+                return new TransactionResults(
+                    request.PageNumber,
+                    request.PageSize,
+                    totalResults,
+                    transactions);
+            }
+        }
+    }
+}
